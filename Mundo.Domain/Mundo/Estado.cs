4466e51c8c8aff19bc1bc68c2cc3a12d171b0edf
Duarte <guilhermeduartt21@gmail.com>
2020-09-23 15:48:24 -0300

    Adicionar arquivos de projeto.

---
+++ Mundo.Domain/Mundo/Estado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundo.Domain.Mundo
+{
+    public class Estado
+    {
+        public Guid Id { get; set; }
+        public string UrlFoto { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Mundo.Domain/Mundo/Pais.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundo.Domain.Mundo
+{
+    public class Pais
+    {
+        public Guid Id { get; set; }
+        public string UrlFoto { get; set; }
+        public string Nome { get; set; }
+        public virtual List<Estado> Estados { get; set; } = new List<Estado>();
+    }
+}
