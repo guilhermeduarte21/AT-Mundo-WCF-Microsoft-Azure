9feec90ea9ade378aee666ddce86a5c223540946
Duarte <guilhermeduartt21@gmail.com>
2020-09-25 01:49:10 -0300

    feat: Home

--- Mundo.Web/Controllers/HomeController.cs
+++ Mundo.Web/Controllers/HomeController.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mundo.Web.Models;
+using Mundo.Web.Models.Estado;
 using Mundo.Web.Models.Home;
+using Mundo.Web.Models.Pais;
+using Mundo.Web.Models.Pessoa;
+using RestSharp;
 
 namespace Mundo.Web.Controllers
 {
@@ -21,11 +25,24 @@ namespace Mundo.Web.Controllers
 
         public IActionResult Index()
         {
+            var client = new RestClient();
+
+            var requestEstado = new RestRequest("https://localhost:44310/api/Estados");
+            var responseEstado = client.Get<List<EstadoViewModel>>(requestEstado);
+
+
+            var requestPais = new RestRequest("https://localhost:44310/api/Pais");
+            var responsePais = client.Get<List<ListarPaisViewModel>>(requestPais);
+
+            var requestAmigos = new RestRequest("https://localhost:44311/api/Amigos");
+            var responseAmigos = client.Get<List<ListarPessoasViewModel>>(requestAmigos);
+
+
             var pgInicial = new PaginaInicialViewModel
             {
-                QuantidadeAmigos = 0,
-                QuantidadeEstados = 0,
-                QuantidadePaises = 0
+                QuantidadeAmigos = responseAmigos.Data.Count,
+                QuantidadeEstados = responseEstado.Data.Count,
+                QuantidadePaises = responsePais.Data.Count
             };
 
             return View(pgInicial);
